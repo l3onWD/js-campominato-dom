5d383fe80504dfd6e127508a5677c8065a451d7b
l3on <l3onwebdev@gmail.com>
2023-06-08 17:58:33 +0200

    deleted single cell bomb update and fixed comments

--- js/script.js
+++ js/script.js
@@ -1,45 +1,12 @@
-/*
-Il computer deve generare 16 numeri casuali nello stesso range della difficltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell'array delle bombe non potranno esserci due numeri uguali
-
-In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati abbiamo calpestato una bomba. La cella si colora di rosso e la partita termina. Altrimenti, la cella cliccata si colora di azzurro e l'utente può continuare  a cliccare sulle altre celle.
-
-LA partita termina quando il giocatore clicca su una bomba o quando raggiunger il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
-
-Al termine della partita, il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha cliccato su una cella che non era una bomba
-
-
-# MILESTONE 1
-    - Prepariamo "Qualcosa" per tenere il punteggio dell'utente.
-    - Quando l'utente clicca su una cella, incrementiamo il punteggio.
-    - Se riusciamo, facciamo anche in modo da non poter più cliccare sulla stessa cella
-
-# MILESTONE 2
-    - Facciamo in modo di generare 16 numeri casuali (tutti diversi) compresi tra 1 e il massimo di caselle disponibili.
-    - Generiamoli e stampiamo in console per essere certi che siano corretti
-
-# MILESTONE 3
-    - Quando l'utente clicca su una cella, verrifichiamo se ha calpestato una bomba, controllando se il numero di cella è presente nell'array di bombe.
-    - Se si, la cella diventa rossa (raccogliamo il punteggio e scriviamo in console che la patita termina) altrimenti diventa azzurra e dobbiamo incrementare il punteggio.
-
-# MILESTONE 4
-    - Quando l'utente clicca su una cella, e questa non è una bomba, dobbiamo controllare se il punteggio incrementato ha raggiunto l punteggio massimo, perchè in quel caso la partita termina. Raccogliamo quindi il messaggio e scriviamo un messaggio appropriato.
-
-# MILESTONE 5
-    - Quando la partita termina dobbiamo capire se è terminata perchè è stata cliccata una bomba o seperchè l'utente ha raggiunto il punteggio massimo(ossia ha vinto). Dobbiamo poi in ogni caso stampare lin pagina il punteggio raggiunto ed il messaggio adeguato in caso di vittoria o sconfitta.
-
-# BONUS
-    - Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà (come le istruzioni di ieri se non già fatto)
-
-# SUPERBONUS
-    - Colorare tutte le celle bomba quando la partita finisce
-*/
-
 /* -----------------------------------------
 * FUNCTIONS
 -------------------------------------------*/
 
 //*** START GAME ***//
 const playGame = () => {
+    /* EXTERNAL VARIABLES
+        - all DOM elements
+    */
 
     /* --------------
     * FUNCTIONS
@@ -75,7 +42,7 @@ const playGame = () => {
 
             } while (bombs.includes(randomNumber));
 
-            // Add unique number inside the list
+            // Add unique cell inside the list
             bombs.push(randomNumber);
         }
 
@@ -85,12 +52,15 @@ const playGame = () => {
 
     //*** SHOW END MESSAGE ***//
     const showEndMessage = (score, hasWon) => {
+        /* EXTERNAL VARIABLES
+            - messageElem
+        */
 
         // Prepare paragraphs
         let msg = '<p class="mb-2">HAI VINTO!!</p>';
         const scoreMsg = `<p>Il tuo punteggio è ${score} punti!</p>`
         
-        // Check if is a victory or a lost
+        // Check if is a lost
         if(!hasWon) msg = '<p class="mb-2">HAI PERSO!!</p>';
 
         // Create message
@@ -127,11 +97,18 @@ const playGame = () => {
     //*** ON CELL CLICK ***//
     const onCellClick = (ev) => {
 
+        /* EXTERNAL VARIABLES
+            - gameEnded
+            - bombs
+            - maxScore
+            - score
+        */
+
         //*** CHECK IF GAME ENDED ***//
         if(gameEnded) return;
 
 
-        //*** CHECK IF CLICKED ***//
+        //*** CHECK IF ALREADY CLICKED ***//
         // Get current cell clicked
         const cell = ev.target;
 
@@ -148,7 +125,7 @@ const playGame = () => {
         // Get cell number
         const cellNumber = parseInt(cell.innerText);
 
-        
+        // Is a bomb cell 
         if(bombs.includes(cellNumber)) {
 
             //*** GAME LOST ***//
@@ -157,8 +134,6 @@ const playGame = () => {
 
             // Show all cells
             showAllCells(bombs);
-
-            cell.classList.add('bomb');
 
             // Show a message
             showEndMessage(score, false);
@@ -187,12 +162,17 @@ const playGame = () => {
     * INIT
     ----------------*/
 
-    //*** DATA ***//
-    // Game score
-    let score = 0;
+    //*** PARAMETERS ***//
+    // Game difficulty
+    const cellsPerRowEasy = 10;
+    const cellsPerRowMedium = 9;
+    const cellsPerRowHard = 7;
 
     // Game bombs
     const maxBombs = 16;
+
+    // Game score
+    let score = 0;
 
     // Game Status
     let gameEnded = false;
@@ -276,12 +256,6 @@ const difficultyElem = document.getElementById('game-difficulty');
 const playBtn = document.getElementById('game-play-btn');
 const gridElem = document.getElementById('game-grid');
 const messageElem = document.getElementById('game-message');
-
-
-//*** PARAMETERS ***//
-const cellsPerRowEasy = 10;
-const cellsPerRowMedium = 9;
-const cellsPerRowHard = 7;
 
 
 // ! Log
